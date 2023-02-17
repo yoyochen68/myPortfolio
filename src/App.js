219267cc39d69533bee66ef79956dd6094c2e2ea
yoyochen68 <90425062+yoyochen68@users.noreply.github.com>
2023-02-17 03:08:01 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="../logo-leaf-tran-one.png" className="" alt="logo" />
+   
+        <h1>
+          Under Construction ...
+        </h1>
+       
       </header>
     </div>
   );
