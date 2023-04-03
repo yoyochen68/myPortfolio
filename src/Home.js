2c1fb4780a14f5349e7efc46ac448f520f87bcc5
yoyochen68 <90425062+yoyochen68@users.noreply.github.com>
2023-04-03 13:45:51 -0700

    update home page

--- src/Home.js
+++ src/Home.js
@@ -22,7 +22,7 @@ function Home() {
           <p className='desText'> I thrive on exploring the infinite possibilities of Web Development and UX/UI Design </p>
           <p className='desText'> I am actively seeking opportunities to utilize my full stack web development skills to create impactful solutions.
           </p>
-          <p className='desText'>If you have any questions or just want to say hi, feel free to drop me a message. My inbox is always open!</p>
+          <p className='desText'>If you have any questions or just want to say hi, feel free to drop me a message. My inbox is always open!<a href="mailto:yoyochen2019@yahoo.ca?Subject=I%20want%20to%20connect"><img src="../img/email.png" className="siteEmailLogo" alt="logo" /></a></p>
         </section>
       </div>
       <Footer />
